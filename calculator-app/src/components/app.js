e1b4aca84949e446fdcc98380d7ac4da6be72ad4
morganbarlow <morgan.barlow2232@gmail.com>
2021-03-24 22:31:19 -0600

    adding styles

--- calculator-app/src/components/app.js
+++ calculator-app/src/components/app.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import React, { Component } from 'react';
 
+
 function App() {
   const [result, setResult] = useState('')
   return(
@@ -12,35 +13,39 @@ function App() {
       </div>
 
       <div className = "button-display">
-        <div className = "top-row">
-          <button>CE</button>
-          <button>C</button>
-          <button>del</button>
-          <button>/</button>
-        </div>
-        <div className = "second-row">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>*</button>
-        </div>
-        <div className = "third-row">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-        </div>
-        <div className = "fourth-row">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-        </div>
-        <div className = "bottom-row">
-          <button>+/-</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+        <div className ="button-row">
+          
+          <div className = "buttons">
+            <button>CE</button>
+            <button>C</button>
+            <button>del</button>
+            <button>/</button>
+          </div>
+          <div className = "buttons">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>*</button>
+          </div>
+          <div className = "buttons">
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+          </div>
+          <div className = "buttons">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+          </div>
+          <div className = "buttons">
+            <button>+/-</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
+
         </div>
       </div>
     </div>
